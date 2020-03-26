7c44a63244884a92453345787ee23c836b9d7d8c
tom keane <tomkeane07@gmail.com>
2020-03-26 10:37:13 +0000

    .gitignore fix

--- tutorial_02/app/build/generated/source/dataBinding/trigger/debug/com/example/aboutme/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.example.aboutme;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
--- tutorial_03/app/build/tmp/kapt3/stubs/debug/com/example/android/navigation/MainActivityKt.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.navigation;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\b\n\u0000\n\u0002\u0018\u0002\n\u0000\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0002"}, d2 = {"drawerLayout", "Landroidx/drawerlayout/widget/DrawerLayout;", "app_debug"})
-public final class MainActivityKt {
-    private static androidx.drawerlayout.widget.DrawerLayout drawerLayout;
-}
