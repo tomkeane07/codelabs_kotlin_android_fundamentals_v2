5220a3c55b9f34cc41fa3f5e21be32bd59fd2f05
tom keane <tomkeane07@gmail.com>
2020-03-30 11:12:04 +0100

    play again funct

--- tutorial_05/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ tutorial_05/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,13 +1,27 @@
 package com.example.android.guesstheword.screens.score
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel(finalScore: Int) : ViewModel() {
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
     // final score
     var score = finalScore
     init{
         Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
+
+    fun onPlayAgain(){
+        _eventPlayAgain.value = true
+    }
+
+    fun onPlayaAgainComplete(){
+        _eventPlayAgain.value = false
     }
 
 }
