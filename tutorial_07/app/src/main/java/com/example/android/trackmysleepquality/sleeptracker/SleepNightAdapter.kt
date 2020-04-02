e2ce574c5680ab9f431f513e33dacb21002d5d1e
tom keane <tomkeane07@gmail.com>
2020-04-02 16:04:59 +0100

    Implemented SleepNightDiffCallback, ListAdapter manages list

--- tutorial_07/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ tutorial_07/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,3 +1,16 @@
+/*
+The SleepNightAdapter adapts the list of SleepNight objects
+ into something RecyclerView can use and display.
+
+ The SleepNightAdapter adapter produces ViewHolders that contain
+  the views, data, and meta information for the recycler view to display the data.
+
+  RecyclerView uses the SleepNightAdapter to determine how many items
+   there are to display (getItemCount()).
+    RecyclerView uses onCreateViewHolder()
+   and onBindViewHolder() to get view holders bound to data for displaying.
+ */
+
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.graphics.Color
@@ -6,6 +19,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
@@ -13,20 +28,23 @@ import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
+class SleepNightAdapter: ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
-    var data =  listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
+    class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
+        override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem.nightId == newItem.nightId
         }
 
-    override fun getItemCount() = data.size
+        override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem == newItem
+        }
+
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         //display the data for one list item at the specified position
 
-        val item = data[position]
+        val item = getItem(position)
         holder.bind(item)
     }
 
