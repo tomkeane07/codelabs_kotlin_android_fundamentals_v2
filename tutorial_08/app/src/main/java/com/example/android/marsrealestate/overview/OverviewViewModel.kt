5b982539efcbc3c0d572514febbb85722f0534cb
tom keane <tomkeane07@gmail.com>
2020-04-07 03:30:12 +0100

    added moshi parsing

--- tutorial_08/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ tutorial_08/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsAPI
+import com.example.android.marsrealestate.network.MarsProperty
 import retrofit2.Call
 import retrofit2.Response
 import javax.security.auth.callback.Callback
@@ -49,14 +50,14 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsRealEstateProperties() {
         MarsAPI.retrofitService.getProperties().enqueue(
-                object: Callback, retrofit2.Callback<String> {
-                    override fun onFailure(call: Call<String>, t: Throwable) {
+                object: Callback, retrofit2.Callback<List<MarsProperty>> {
+                    override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
                         _response.value = "Failure " + t.message
                     }
 
-                    override fun onResponse(call: Call<String>,
-                                            response: Response<String>) {
-                        _response.value = response.body()
+                    override fun onResponse(call: Call<List<MarsProperty>>,
+                                            response: Response<List<MarsProperty>>) {
+                        _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
                     }
 
                 })
